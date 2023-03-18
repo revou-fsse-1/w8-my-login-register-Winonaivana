0f107c5d35814ad549f202f764b5907018e66f21
winona ivana <winonaivana@gmail.com>
2023-03-18 07:13:28 +0000

    update

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ var emailError = document.getElementById("email-error");
 var passwordError = document.getElementById("password-error");
 var emailError1 = document.getElementById("email-error1");
 var passwordError1 = document.getElementById("password-error1");
+var loginBox = document.getElementById("form-boxlogin");
+var regBox = document.getElementById("form-boxregister");
 
 registerLink.addEventListener("click", () => {
   wrapper.classList.add("active");
@@ -25,12 +27,7 @@ loginLink.addEventListener("click", () => {
 btnPop.addEventListener("click", () => {
   wrapper.classList.add("active-popup");
   navigation.classList.add("active-appear");
-  wrapper1.classList.add("active-popup1");
-});
-
-btnPop1.addEventListener("click", () => {
-  wrapper1.classList.add("active-popup2");
-  navigation.classList.add("active-appear");
+  image.classList.add("active-appearr");
 });
 
 logo.addEventListener("click", () => {
@@ -41,161 +38,71 @@ logo.addEventListener("click", () => {
 close.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
   navigation.classList.remove("active-appear");
-  wrapper1.classList.remove("active-popup1");
+  image.classList.remove("active-appearr");
 });
 
-function validateName() {
-  var name = document.getElementById("username").value;
+function store() {
+  var storedEmail = localStorage.getItem("email");
 
-  if (name.length == 0) {
-    nameError.innerHTML = "Name is required";
-    return false;
-  }
-  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
-    nameError.innerHTML = "Full Name required";
-    return false;
-  }
-  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-  return true;
-}
+  var email = document.getElementById("re");
+  var password = document.getElementById("rp");
+  var lowerCaseLetters = /[a-z]/g;
+  var upperCaseLetters = /[A-Z]/g;
+  var numbers = /[0-9]/g;
 
-function validateEmail() {
-  var email = document.getElementById("emailInput").value;
-
-  if (email.length == 0) {
-    emailError.innerHTML = "Email is required";
-    return false;
-  }
-  if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-    emailError.innerHTML = "Must available email";
-    return false;
-  }
-  emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-  return true;
-}
-
-function validatePassword() {
-  var password = document.getElementById("passwordInput").value;
-
-  if (password.length == 0) {
-    passwordError.innerHTML = "Password required";
-    return false;
-  }
-
-  if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)) {
-    passwordError.innerHTML = "At least 1 uppercase, 1 lowercase, 1 number";
-    return false;
-  }
-  passwordError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-  return true;
-}
-
-function validateEmail1() {
-  var email1 = document.getElementById("emailInput1").value;
-
-  if (email1.length == 0) {
-    emailError1.innerHTML = "Email is required";
-    return false;
-  }
-  if (!email1.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-    emailError1.innerHTML = "Must available email";
-    return false;
-  }
-  emailError1.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-  return true;
-}
-
-function validatePassword1() {
-  var password1 = document.getElementById("passwordInput1").value;
-
-  if (password1.length == 0) {
-    passwordError1.innerHTML = "Password required";
-    return false;
-  }
-
-  if (!password1.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)) {
-    passwordError1.innerHTML = "At least 1 uppercase, 1 lowercase, 1 number";
-    return false;
-  }
-  passwordError1.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-  return true;
-}
-
-var row = null;
-
-function Submit() {
-  var dataEntered = retrieveData();
-  var readData = readingData(dataEntered);
-  if (dataEntered == false) {
-    msg.innerHTML = "";
+  if (email.value.length == 0) {
+    alert("Please fill in email");
+  } else if (password.value.length == 0) {
+    alert("Please fill in password");
+  } else if (email.value.length == 0 && password.value.length == 0) {
+    alert("Please fill in email and password");
+  } else if (password.value.length > 8) {
+    alert("Max of 8");
+  } else if (!password.value.match(numbers)) {
+    alert("please add 1 number");
+  } else if (!password.value.match(upperCaseLetters)) {
+    alert("please add 1 uppercase letter");
+  } else if (!password.value.match(lowerCaseLetters)) {
+    alert("please add 1 lowercase letter");
+  } else if (email.value == storedEmail) {
+    alert("email already exist");
   } else {
-    if (row == null) {
-      insert(readData);
-      msg.innerHTML = "";
-    } else {
-      update();
-      msg.innerHTML = "";
-    }
+    localStorage.setItem("email", email.value);
+    localStorage.setItem("password", password.value);
+    alert("Your account has been created");
   }
-  document.getElementById("form").reset();
 }
 
-// CREATE
-function retrieveData() {
-  var username = document.getElementById("username").value;
-  var email = document.getElementById("emailInput1").value;
-  var password = document.getElementById("passwordInput1").value;
+function check() {
+  var storedEmail = localStorage.getItem("email");
+  var storedPassword = localStorage.getItem("password");
 
-  var arr = [username, email, password];
-  if (arr.includes("")) {
-    return false;
+  var userEmail = document.getElementById("se");
+  var userPassword = document.getElementById("sp");
+
+  if (userEmail.value == storedEmail && userPassword.value == storedPassword) {
+    alert("You are logged in.");
   } else {
-    return arr;
+    alert("Error on login");
   }
 }
 
-// READ
-function readingData(dataEntered) {
-  var u = localStorage.setItem("username", dataEntered[0]);
-  var e = localStorage.setItem("email", dataEntered[1]);
-  var p = localStorage.setItem("password", dataEntered[2]);
+function bb() {
+  var storedEmail = localStorage.getItem("email");
 
-  var u1 = localStorage.getItem("username", u);
-  var e1 = localStorage.getItem("email", e);
-  var p1 = localStorage.getItem("password", p);
+  var userEmail = document.getElementById("re");
 
-  var arr = [u1, e1, p1];
-  return arr;
+  if (userEmail.value == storedEmail) {
+    alert("Email used");
+  } else {
+    store();
+  }
 }
 
-// INSERT
-function insert(readData) {
-  var row = table.insertRow();
-  row.insertCell(0).innerHTML = readData[0];
-  row.insertCell(1).innerHTML = readData[1];
-  row.insertCell(2).innerHTML = readData[2];
-  row.insertCell(3).innerHTML = `<button onclick = edit(this)>Edit</button>
-    <button onclick = remove(this)>Delete</button>`;
+function register() {
+  store();
 }
 
-//EDIT
-function edit(td) {
-  row = td.parentElement.parentElement;
-  document.getElementById("username").value = row.cells[0].innerHTML;
-  document.getElementById("emailInput1").value = row.cells[1].innerHTML;
-  document.getElementById("passwordInput1").value = row.cells[2].innerHTML;
-}
-
-// UPDATE
-function update(td) {
-  row.cells[0].innerHTML = document.getElementById("username").value;
-  row.cells[1].innerHTML = document.getElementById("emailInput1").value;
-  row.cells[2].innerHTML = document.getElementById("passwordInput1").value;
-  row = null;
-}
-
-// DELETE
-function remove(td) {
-  row = td.parentElement.parentElement;
-  document.getElementById("table").deleteRow(row.rowIndex);
+function login() {
+  check();
 }
